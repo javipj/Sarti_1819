33fde0d6bb08780d3bc0a2c3e3f29133fc0633b6
javier <javipj@gmail.com>
2019-04-12 09:32:16 +0200

    c

--- exercici_suma_min_max_funcions.php
+++ exercici_suma_min_max_funcions.php
@@ -9,14 +9,31 @@ function generaarray($longitud){
     return   $numeros;
 }
 function _max($array){
+    $max=$array[0];
+    foreach($array as $numero){
+        if($numero>$max) $max=$numero;
+    }
+    return $max;
 
-
-    
 }
-function _min($array){}
-function _suma($array){}
+function _min($array){
+    $min=$array[0];
+    foreach($array as $numero){
+        if($numero<$min) $min=$numero;
+    }
+    return $min;
+}
+function _suma($array){
+    $sum=0;
+    foreach($array as $numero){
+        $sum+=$numero;
+    }
+    return $sum;
+}
+
 
 $numeros=generaarray(10);
+print_r($numeros);
 echo "<br>El max es "._max($numeros);
 echo "<br>El min es "._min($numeros);
 echo "<br>La suma es "._suma($numeros);
