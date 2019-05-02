b318a74d41a6822880fbaa7446799aa506042b35
javipj <javipj@gmail.com>
2019-05-02 13:55:35 +0200

    test

--- uf1/part2/ejemplo_privado.php
+++ uf1/part2/ejemplo_privado.php
@@ -3,6 +3,9 @@ session_start();
 
 if(isset($_REQUEST["logout"])){
     session_destroy(); 
+    setcookie("password",0,1);
+    setcookie("nomusuari",0,1);
+
     header('Location:ejemplo_login.php');           
 
 }
@@ -10,7 +13,7 @@ if(isset($_REQUEST["logout"])){
 
 
 
-if(isset($_SESSION["login"])){
+if(isset($_SESSION["login"])&&$_SESSION["login"]==true){
 ?>
 Bienvenido......<?=$_SESSION["nom"]?>
 
